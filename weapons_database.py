764ee41ad3ed9b97e75bf6bde0ea53ace16b19c5
Oleh Novikov <novikoff10000@gmail.com>
2025-04-27 14:11:25 +0300

    Add weapons_database module to load weapons_db.json

--- weapons_database.py
+++ weapons_database.py
@@ -1,6 +1,10 @@
 import json
+import os
 
-with open("weapons_db.json", "r", encoding="utf-8") as file:
-    weapons_data = json.load(file)
+# Знаходимо абсолютний шлях до файлу JSON
+base_dir = os.path.dirname(os.path.abspath(__file__))
+json_path = os.path.join(base_dir, "weapons_db.json")
 
-
+# Відкриваємо файл та читаємо дані
+with open(json_path, "r", encoding="utf-8") as f:
+    weapons_data = json.load(f)
